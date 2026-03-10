0b3a4779c0e76a1914d70635964523862d7855dc
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2026-03-10 22:38:25 +0200

    Update views.py
    fixing missing " e" letter 
    I forgot to add the letter ‘e.

--- services/core-api/projects/views.py
+++ services/core-api/projects/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from rest_framework import generics, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framwork.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from livekit import api
 
 from .models import Project
